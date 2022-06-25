8fe904cd5dc87fbb44911b2bdcc953f1edf2654d
garbhagudi <garbhagudi.11@gmail.com>
2022-06-25 17:59:43 +0530

    Ads Conversion Fix 1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,8 @@ import * as ga from "../lib/google-analytics";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Script from "next/script";
-import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
-  const tagManagerArgs = {
-    gtmId: "AW-681967532",
-  };
-  TagManager.initialize(tagManagerArgs);
-
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: any) => {
@@ -50,6 +44,10 @@ function MyApp({ Component, pageProps }) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ADS_CONVERSION_ID}`}
+        strategy="afterInteractive"
+      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -57,6 +55,15 @@ function MyApp({ Component, pageProps }) {
           gtag('js', new Date());
 
           gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GOOGLE_ADS_CONVERSION_ID}');
         `}
       </Script>
       <Header />
