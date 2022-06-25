fa0f812d823546e2b4c4553a37f5ac212c8e871e
garbhagudi <garbhagudi.11@gmail.com>
2022-06-25 18:07:50 +0530

    ads conversion fix 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,10 +44,6 @@ function MyApp({ Component, pageProps }) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ADS_CONVERSION_ID}`}
-        strategy="afterInteractive"
-      />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -55,15 +51,6 @@ function MyApp({ Component, pageProps }) {
           gtag('js', new Date());
 
           gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
-        `}
-      </Script>
-      <Script id="google-ads" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.GOOGLE_ADS_CONVERSION_ID}');
         `}
       </Script>
       <Header />
