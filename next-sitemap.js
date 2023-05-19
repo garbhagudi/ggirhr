e31e05a30c75e6ed4fbd1b0d95a77a70db4b862a
garbhagudi <garbhagudi.11@gmail.com>
2023-05-19 13:07:50 +0530

    sitemap fixes

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://www.ggirhr.com",
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 20000,
-};
