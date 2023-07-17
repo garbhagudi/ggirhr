8aa1ae08df4a3ee2e20c84551c24f75e02c3533d
garbhagudi <garbhagudi.11@gmail.com>
2023-07-17 12:38:57 +0530

    minor corrections

--- next.config.js
+++ next.config.js
@@ -2,25 +2,25 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ['res.cloudinary.com', 'media.graphassets.com'],
   },
   async rewrites() {
     return [
       {
-        source: "/public/contact.html",
-        destination: "/src/pages/api/contact.tsx",
+        source: '/public/contact.html',
+        destination: '/src/pages/api/contact.tsx',
       },
       {
-        source: "/public/contact.html",
-        destination: "/src/pages/api/contact.tsx",
+        source: '/public/contact.html',
+        destination: '/src/pages/api/contact.tsx',
       },
     ];
   },
   async redirects() {
     return [
       {
-        source: "/courses/comprehensive-untrasound-scan-course",
-        destination: "/courses/comprehensive-tvs-ultrasound-scan-course",
+        source: '/courses/comprehensive-untrasound-scan-course',
+        destination: '/courses/comprehensive-tvs-ultrasound-scan-course',
         permanent: true,
       },
     ];
