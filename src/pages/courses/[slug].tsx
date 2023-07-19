59a5f26c5d1d7736909f9ab0128f52ad5b62b410
garbhagudi <garbhagudi.11@gmail.com>
2023-07-19 15:07:59 +0530

    phone number change

--- src/pages/courses/[slug].tsx
+++ src/pages/courses/[slug].tsx
@@ -78,7 +78,7 @@ const CoursePage = ({ course }) => {
             </div>
             <div className='rounded-md shadow flex justify-center'>
               <a
-                href='tel:+917204937376'
+                href='tel:+919108910852'
                 className='w-full flex items-center justify-center px-5 py-3 bg-white border border-transparent text-base font-medium rounded-md text-brandBlue'
               >
                 <PhoneIcon className='w-5 h-5 mr-2' /> Call Us
@@ -101,7 +101,7 @@ const CoursePage = ({ course }) => {
                 </div>
                 <div className='rounded-md shadow flex justify-center'>
                   <a
-                    href='tel:+917204937376'
+                    href='tel:+919108910852'
                     className='w-full flex items-center justify-center px-5 py-3 bg-white border border-transparent text-base font-medium rounded-md text-brandBlue'
                   >
                     <PhoneIcon className='w-5 h-5 mr-2' /> Call Us
