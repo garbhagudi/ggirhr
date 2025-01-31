d0fd5b372c572afc4dfb01088822e8a75e935ea8
Tamil Arasan <tamil@witarist.com>
2025-01-31 17:43:17 +0530

    HR contact number removed from the websites

--- src/sections/gg-care/content.tsx
+++ src/sections/gg-care/content.tsx
@@ -1,41 +1,41 @@
-import { AtSymbolIcon, PhoneIcon } from '@heroicons/react/solid';
-import Link from 'next/link';
+import { AtSymbolIcon, PhoneIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 
 const Contact = () => {
   return (
-    <div className='bg-white'>
-      <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8'>
-        <div className='divide-y-2 divide-gray-200'>
-          <div className='lg:grid lg:grid-cols-3 lg:gap-8'>
-            <h2 className='text-2xl font-extrabold text-gray-900 sm:text-3xl font-heading'>
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+        <div className="divide-y-2 divide-gray-200">
+          <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+            <h2 className="text-2xl font-extrabold text-gray-900 sm:text-3xl font-heading">
               Get in touch
             </h2>
-            <div className='mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2 font-content'>
+            <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2 font-content">
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Academics & Queries
                 </h3>
-                <dl className='mt-2 text-base text-gray-500'>
+                <dl className="mt-2 text-base text-gray-500">
                   <div>
-                    <dt className='sr-only'>Email</dt>
+                    <dt className="sr-only">Email</dt>
                     <dd>
                       <Link
-                        href='mailto:training@ggirhr.com'
-                        className='text-brandPink2 hover:underline'
+                        href="mailto:training@ggirhr.com"
+                        className="text-brandPink2 hover:underline"
                       >
-                        <AtSymbolIcon className='inline-flex w-5 h-5' />{' '}
+                        <AtSymbolIcon className="inline-flex w-5 h-5" />{" "}
                         training@ggirhr.com
                       </Link>
                     </dd>
                   </div>
-                  <div className='mt-1'>
-                    <dt className='sr-only'>Phone number</dt>
+                  <div className="mt-1">
+                    <dt className="sr-only">Phone number</dt>
                     <dd>
                       <Link
-                        href='tel:+919108910852'
-                        className='text-brandBlue hover:underline'
+                        href="tel:+919108910852"
+                        className="text-brandBlue hover:underline"
                       >
-                        <PhoneIcon className='inline-block w-5 h-5' />{' '}
+                        <PhoneIcon className="inline-block w-5 h-5" />{" "}
                         +919108910852
                       </Link>
                     </dd>
@@ -43,30 +43,30 @@ const Contact = () => {
                 </dl>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Feedback & Complaints
                 </h3>
-                <dl className='mt-2 text-base text-gray-500'>
+                <dl className="mt-2 text-base text-gray-500">
                   <div>
-                    <dt className='sr-only'>Email</dt>
+                    <dt className="sr-only">Email</dt>
                     <dd>
                       <Link
-                        href='mailto:manager@ggirhr.com'
-                        className='text-brandPink2 hover:underline'
+                        href="mailto:manager@ggirhr.com"
+                        className="text-brandPink2 hover:underline"
                       >
-                        <AtSymbolIcon className='inline-flex w-5 h-5' />{' '}
+                        <AtSymbolIcon className="inline-flex w-5 h-5" />{" "}
                         manager@ggirhr.com
                       </Link>
                     </dd>
                   </div>
-                  <div className='mt-1'>
-                    <dt className='sr-only'>Phone number</dt>
+                  <div className="mt-1">
+                    <dt className="sr-only">Phone number</dt>
                     <dd>
                       <Link
-                        href='tel:+919108910852'
-                        className='text-brandBlue hover:underline'
+                        href="tel:+919108910852"
+                        className="text-brandBlue hover:underline"
                       >
-                        <PhoneIcon className='inline-block w-5 h-5' />{' '}
+                        <PhoneIcon className="inline-block w-5 h-5" />{" "}
                         +919108910852
                       </Link>
                     </dd>
@@ -74,61 +74,49 @@ const Contact = () => {
                 </dl>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Human Resources
                 </h3>
-                <dl className='mt-2 text-base text-gray-500'>
+                <dl className="mt-2 text-base text-gray-500">
                   <div>
-                    <dt className='sr-only'>Email</dt>
+                    <dt className="sr-only">Email</dt>
                     <dd>
                       <Link
-                        href='mailto:hr@garbhagudi.com'
-                        className='text-brandPink2 hover:underline'
+                        href="mailto:hr@garbhagudi.com"
+                        className="text-brandPink2 hover:underline"
                       >
-                        <AtSymbolIcon className='inline-flex w-5 h-5' />{' '}
+                        <AtSymbolIcon className="inline-flex w-5 h-5" />{" "}
                         hr@garbhagudi.com
-                      </Link>
-                    </dd>
-                  </div>
-                  <div className='mt-1'>
-                    <dt className='sr-only'>Phone number</dt>
-                    <dd>
-                      <Link
-                        href='tel:+919606002999'
-                        className='text-brandBlue hover:underline'
-                      >
-                        <PhoneIcon className='inline-block w-5 h-5' /> +91 96060
-                        02999
                       </Link>
                     </dd>
                   </div>
                 </dl>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Partnership
                 </h3>
-                <dl className='mt-2 text-base text-gray-500'>
+                <dl className="mt-2 text-base text-gray-500">
                   <div>
-                    <dt className='sr-only'>Email</dt>
+                    <dt className="sr-only">Email</dt>
                     <dd>
                       <Link
-                        href='mailto:jayaram@garbhagudi.com'
-                        className='text-brandPink2 hover:underline'
+                        href="mailto:jayaram@garbhagudi.com"
+                        className="text-brandPink2 hover:underline"
                       >
-                        <AtSymbolIcon className='inline-flex w-5 h-5' />{' '}
+                        <AtSymbolIcon className="inline-flex w-5 h-5" />{" "}
                         jayaram@garbhagudi.com
                       </Link>
                     </dd>
                   </div>
-                  <div className='mt-1'>
-                    <dt className='sr-only'>Phone number</dt>
+                  <div className="mt-1">
+                    <dt className="sr-only">Phone number</dt>
                     <dd>
                       <Link
-                        href='tel:+919980997111'
-                        className='text-brandBlue hover:underline'
+                        href="tel:+919980997111"
+                        className="text-brandBlue hover:underline"
                       >
-                        <PhoneIcon className='inline-block w-5 h-5' /> +91 99809
+                        <PhoneIcon className="inline-block w-5 h-5" /> +91 99809
                         97111
                       </Link>
                     </dd>
@@ -137,22 +125,22 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <div className='mt-16 pt-16 lg:grid lg:grid-cols-3 lg:gap-8'>
-            <h2 className='text-2xl font-extrabold text-gray-900 sm:text-3xl font-heading'>
+          <div className="mt-16 pt-16 lg:grid lg:grid-cols-3 lg:gap-8">
+            <h2 className="text-2xl font-extrabold text-gray-900 sm:text-3xl font-heading">
               Locations
             </h2>
-            <div className='mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2 font-content'>
+            <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2 font-content">
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Hanumanthanagar
                 </h3>
-                <div className='mt-2 text-base text-gray-500'>
-                  <p className='mt-1'>
+                <div className="mt-2 text-base text-gray-500">
+                  <p className="mt-1">
                     <Link
-                      href='tel:+919886915556'
-                      className='text-brandBlue hover:underline'
+                      href="tel:+919886915556"
+                      className="text-brandBlue hover:underline"
                     >
-                      <PhoneIcon className='inline-block w-5 h-5' /> +91 98869
+                      <PhoneIcon className="inline-block w-5 h-5" /> +91 98869
                       15556
                     </Link>
                   </p>
@@ -160,28 +148,28 @@ const Contact = () => {
                     Subhasri Complex, 210/A, Kumaraswamy Temple Rd, 2nd Block,
                     Phase 1, Banashankari Stage I, Banashankari,
                   </p>
-                  <p className='mt-1 mb-5'>Bengaluru, Karnataka 560050</p>
+                  <p className="mt-1 mb-5">Bengaluru, Karnataka 560050</p>
                   <Link
-                    href='https://g.page/GG1-HN?share'
-                    className='py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue'
-                    target={'_blank'}
-                    rel='noreferrer'
+                    href="https://g.page/GG1-HN?share"
+                    className="py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue"
+                    target={"_blank"}
+                    rel="noreferrer"
                   >
                     Directions
                   </Link>
                 </div>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Kalyan Nagar
                 </h3>
-                <div className='mt-2 text-base text-gray-500'>
-                  <p className='mt-1'>
+                <div className="mt-2 text-base text-gray-500">
+                  <p className="mt-1">
                     <Link
-                      href='tel:+918861433339'
-                      className='text-brandBlue hover:underline'
+                      href="tel:+918861433339"
+                      className="text-brandBlue hover:underline"
                     >
-                      <PhoneIcon className='inline-block w-5 h-5' /> +91 88614
+                      <PhoneIcon className="inline-block w-5 h-5" /> +91 88614
                       33339
                     </Link>
                   </p>
@@ -189,56 +177,56 @@ const Contact = () => {
                     Park Landing, 5AC-709, Outer Ring Rd, Balachandra Layout,
                     HRBR Layout 2nd Block, Chelekare, Extension,
                   </p>
-                  <p className='mt-1 mb-5'>Bengaluru, Karnataka 560043</p>
+                  <p className="mt-1 mb-5">Bengaluru, Karnataka 560043</p>
                   <a
-                    href='https://g.page/GarbhaGudi-IVF-Bangalore?share'
-                    className='py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue'
-                    target={'_blank'}
-                    rel='noreferrer'
+                    href="https://g.page/GarbhaGudi-IVF-Bangalore?share"
+                    className="py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue"
+                    target={"_blank"}
+                    rel="noreferrer"
                   >
                     Directions
                   </a>
                 </div>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   South End Circle
                 </h3>
-                <div className='mt-2 text-base text-gray-500'>
-                  <p className='mt-1'>
+                <div className="mt-2 text-base text-gray-500">
+                  <p className="mt-1">
                     <Link
-                      href='tel:+919972463355'
-                      className='text-brandBlue hover:underline'
+                      href="tel:+919972463355"
+                      className="text-brandBlue hover:underline"
                     >
-                      <PhoneIcon className='inline-block w-5 h-5' /> +91 99724
+                      <PhoneIcon className="inline-block w-5 h-5" /> +91 99724
                       63355
                     </Link>
                   </p>
                   <p>
                     South End Circle, 26, Pattalamma Temple Rd, Basavanagudi,
                   </p>
-                  <p className='mt-1 mb-5'>Bengaluru, Karnataka 560004</p>
+                  <p className="mt-1 mb-5">Bengaluru, Karnataka 560004</p>
                   <Link
-                    href='https://g.page/ggivf?share'
-                    className='py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue'
-                    target={'_blank'}
-                    rel='noreferrer'
+                    href="https://g.page/ggivf?share"
+                    className="py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue"
+                    target={"_blank"}
+                    rel="noreferrer"
                   >
                     Directions
                   </Link>
                 </div>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Electronic City
                 </h3>
-                <div className='mt-2 text-base text-gray-500'>
-                  <p className='mt-1'>
+                <div className="mt-2 text-base text-gray-500">
+                  <p className="mt-1">
                     <Link
-                      href='tel:+918861811116'
-                      className='text-brandBlue hover:underline'
+                      href="tel:+918861811116"
+                      className="text-brandBlue hover:underline"
                     >
-                      <PhoneIcon className='inline-block w-5 h-5' /> +91 88618
+                      <PhoneIcon className="inline-block w-5 h-5" /> +91 88618
                       11116
                     </Link>
                   </p>
@@ -246,28 +234,28 @@ const Contact = () => {
                     Ganesh Towers,3rd Floor, 60/5 and 60/6, NH 44, Beside Andhra
                     Bank, Konappana Agrahara, Electronic City,
                   </p>
-                  <p className='mt-1 mb-5'>Bengaluru, Karnataka 560100</p>
+                  <p className="mt-1 mb-5">Bengaluru, Karnataka 560100</p>
                   <Link
-                    href='https://g.page/GG4-EC?share'
-                    className='py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue'
-                    target={'_blank'}
-                    rel='noreferrer'
+                    href="https://g.page/GG4-EC?share"
+                    className="py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue"
+                    target={"_blank"}
+                    rel="noreferrer"
                   >
                     Directions
                   </Link>
                 </div>
               </div>
               <div>
-                <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Marathahalli
                 </h3>
-                <div className='mt-2 text-base text-gray-500'>
-                  <p className='mt-1'>
+                <div className="mt-2 text-base text-gray-500">
+                  <p className="mt-1">
                     <Link
-                      href='tel:+918105181515'
-                      className='text-brandBlue hover:underline'
+                      href="tel:+918105181515"
+                      className="text-brandBlue hover:underline"
                     >
-                      <PhoneIcon className='inline-block w-5 h-5' /> +91 81051
+                      <PhoneIcon className="inline-block w-5 h-5" /> +91 81051
                       81515
                     </Link>
                   </p>
@@ -275,12 +263,12 @@ const Contact = () => {
                     4TH floor, Chirag Towers, Born Babies Building, 24 & 64,
                     Service Rd, next to कलामंदिर, Marathahalli,
                   </p>
-                  <p className='mt-1 mb-5'>Bengaluru, Karnataka 560037</p>
+                  <p className="mt-1 mb-5">Bengaluru, Karnataka 560037</p>
                   <Link
-                    href='https://goo.gl/maps/kxhfM5SCZuq6EDnE9'
-                    className='py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue'
-                    target={'_blank'}
-                    rel='noreferrer'
+                    href="https://goo.gl/maps/kxhfM5SCZuq6EDnE9"
+                    className="py-2 px-3 bg-brandBlue text-white font-semibold rounded-3xl hover:bg-bratext-brandBlue"
+                    target={"_blank"}
+                    rel="noreferrer"
                   >
                     Directions
                   </Link>
