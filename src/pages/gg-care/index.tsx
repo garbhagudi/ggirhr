26d4dc4043987168ad8e402bdadc944d42e9e9a0
ashish-witarist <ashish@witarist.com>
2025-03-18 18:37:42 +0530

    contact us page updated

--- src/pages/gg-care/index.tsx
+++ src/pages/gg-care/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+"use client";
 import Hero from "sections/gg-care/hero";
 import Cta from "sections/gg-care/cta";
 import Video from "sections/gg-care/workflow";
 import Contact from "sections/gg-care/content";
-import Stats from "sections/gg-care/stats";
 import Head from "next/head";
 
 const IndexPage = () => {
@@ -51,7 +50,6 @@ const IndexPage = () => {
       </Head>
       <Hero />
       <Video />
-      <Stats />
       <Contact />
       <Cta />
     </div>
