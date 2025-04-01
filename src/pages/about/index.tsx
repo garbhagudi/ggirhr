19c7d08a1f1365c19ff0708afaf6960c881e4a35
ashish-witarist <ashish@witarist.com>
2025-04-01 18:01:33 +0530

    added CTA and page visited

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import Banner from "sections/About/Banner";
-import Content from "sections/About/content";
-import Stats from "sections/About/Stats";
-import Alumni from "sections/About/Alumni";
-import Cta from "sections/About/Cta";
-import Head from "next/head";
+import React from 'react';
+import Banner from 'sections/About/Banner';
+import Content from 'sections/About/content';
+import Cta from 'sections/About/Cta';
+import Head from 'next/head';
 
 const IndexPage = () => {
   return (
@@ -12,53 +10,53 @@ const IndexPage = () => {
       <Head>
         {/* Primary Tags */}
 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>
           Reproductive Health & Research Institute | Top Infertility Training
           Centre in India
         </title>
         <meta
-          name="title"
-          content="Reproductive Health & Research Institute | Top Infertility Training Centre in India"
+          name='title'
+          content='Reproductive Health & Research Institute | Top Infertility Training Centre in India'
         />
         <meta
-          name="description"
-          content="Leading Reproductive Health Research Institute, among Top Infertility Training Organizations in India, offering best-in-class training at our Infertility Training Centre."
+          name='description'
+          content='Leading Reproductive Health Research Institute, among Top Infertility Training Organizations in India, offering best-in-class training at our Infertility Training Centre.'
         />
 
         {/* Open Graph / Facebook */}
 
         <meta
-          property="og:title"
-          content="Reproductive Health & Research Institute | Top Infertility Training Centre in India"
+          property='og:title'
+          content='Reproductive Health & Research Institute | Top Infertility Training Centre in India'
         />
-        <meta property="og:site_name" content="GGIRHR" />
-        <meta property="og:url" content="https://ggirhr.com" />
+        <meta property='og:site_name' content='GGIRHR' />
+        <meta property='og:url' content='https://ggirhr.com' />
         <meta
-          property="og:description"
-          content="Leading Reproductive Health Research Institute, among Top Infertility Training Organizations in India, offering best-in-class training at our Infertility Training Centre."
+          property='og:description'
+          content='Leading Reproductive Health Research Institute, among Top Infertility Training Organizations in India, offering best-in-class training at our Infertility Training Centre.'
         />
-        <meta property="og:type" content="website" />
+        <meta property='og:type' content='website' />
         <meta
-          property="og:image"
-          content="https://res.cloudinary.com/garbhagudiivf/image/upload/v1651644126/GGIRHR/SEO/SEO_About-min_jifn0w.jpg"
+          property='og:image'
+          content='https://res.cloudinary.com/garbhagudiivf/image/upload/v1651644126/GGIRHR/SEO/SEO_About-min_jifn0w.jpg'
         />
 
         {/* Twitter*/}
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@ggirhr" />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@ggirhr' />
         <meta
-          name="twitter:title"
-          content="Reproductive Health & Research Institute | Top Infertility Training Centre in India"
+          name='twitter:title'
+          content='Reproductive Health & Research Institute | Top Infertility Training Centre in India'
         />
         <meta
-          name="twitter:description"
-          content="Leading Reproductive Health Research Institute, among Top Infertility Training Organizations in India, offering best-in-class training at our Infertility Training Centre."
+          name='twitter:description'
+          content='Leading Reproductive Health Research Institute, among Top Infertility Training Organizations in India, offering best-in-class training at our Infertility Training Centre.'
         />
         <meta
-          name="twitter:image"
-          content="https://res.cloudinary.com/garbhagudiivf/image/upload/v1651644126/GGIRHR/SEO/SEO_About-min_jifn0w.jpg"
+          name='twitter:image'
+          content='https://res.cloudinary.com/garbhagudiivf/image/upload/v1651644126/GGIRHR/SEO/SEO_About-min_jifn0w.jpg'
         />
       </Head>
       <Banner />
--- src/sections/About/content.tsx
+++ src/sections/About/content.tsx
@@ -1,33 +1,33 @@
-import React from "react";
+import React from 'react';
 
 const Data = [
   {
     id: 1,
     description:
-      "Expert team of clinicians trained in reproductive medicine from reputed institutions. They follow standard treatment protocols in ART coupled with innovative process improvements to improve success rates in all aspects of ART. They are committed to conducting prospective and retrospective studies to help improve treatment and ART outcomes.",
+      'Expert team of clinicians trained in reproductive medicine from reputed institutions. They follow standard treatment protocols in ART coupled with innovative process improvements to improve success rates in all aspects of ART. They are committed to conducting prospective and retrospective studies to help improve treatment and ART outcomes.',
   },
   {
     id: 2,
     description:
-      "A capable, knowledgeable, and experienced group of skilled embryologists who follow advanced techniques to improve embryology outcomes. They regularly conduct workshops, participate in conferences, andrology & embryology training programs. They are keen on research activities and conducting studies to analyse, predict and improve IVF outcomes.",
+      'A capable, knowledgeable, and experienced group of skilled embryologists who follow advanced techniques to improve embryology outcomes. They regularly conduct workshops, participate in conferences, andrology & embryology training programs. They are keen on research activities and conducting studies to analyse, predict and improve IVF outcomes.',
   },
   {
     id: 3,
     description:
-      "A team of epidemiologists, statisticians, and research co-coordinators to help formulate, collect data, and analyse the research studies.",
+      'A team of epidemiologists, statisticians, and research co-coordinators to help formulate, collect data, and analyse the research studies.',
   },
   {
     id: 4,
     description:
-      "An internal ethical committee of eminent people in their specified areas as per the DCGI guidelines. Regular meetings of the ethical committee are conducted every 6-8 weeks to discuss and approve new projects, review existing projects (if needed), and suggest any necessary corrections.",
+      'An internal ethical committee of eminent people in their specified areas as per the DCGI guidelines. Regular meetings of the ethical committee are conducted every 6-8 weeks to discuss and approve new projects, review existing projects (if needed), and suggest any necessary corrections.',
   },
   {
     id: 5,
     description:
-      "State of the ART software to collect the patient data and analyse the results to develop actionable insights.",
+      'State of the ART software to collect the patient data and analyse the results to develop actionable insights.',
   },
   {
-    id: 5,
+    id: 6,
     description:
       "We have got full-time staff to digitize the patient's clinical data to help in collecting data for the research work.",
   },
@@ -35,15 +35,15 @@ const Data = [
 
 const Content = () => {
   return (
-    <div className="max-w-7xl mx-auto px-3 sm:px-0">
-      <h1 className="py-16 text-center text-4xl font-heading font-bold text-brandDark">
+    <div className='max-w-7xl mx-auto px-3 sm:px-0'>
+      <h1 className='py-16 text-center text-4xl font-heading font-bold text-brandDark'>
         Our Research Team
       </h1>
-      <ol className="relative border-l border-gray-200 max-w-2xl mx-auto">
+      <ol className='relative border-l border-gray-200 max-w-2xl mx-auto'>
         {Data.map((item) => (
-          <li className="mb-10 ml-4" key={item.id}>
-            <div className="absolute w-3 h-3 bg-gray-400 rounded-full -left-1.5 mt-1.5 border border-white"></div>
-            <p className="mb-4 text-base font-normal text-brandDark">
+          <li className='mb-10 ml-4' key={item.id}>
+            <div className='absolute w-3 h-3 bg-gray-400 rounded-full -left-1.5 mt-1.5 border border-white'></div>
+            <p className='mb-4 text-base font-normal text-brandDark'>
               {item.description}
             </p>
           </li>
